20d4118023175f3197532cc7cc1e91f5067efffe
Ryyyyyyyyu <2458757210@qq.com>
2020-09-21 18:24:55 +0800

    pytest_demo

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     now = time.strftime('%Y.%m.%d-%H_%M_%S-')
     report_dir = now + 'reports'
     report_path = os.path.join(report_path, report_dir)
+    print(report_dir, report_path)
     pytest.main(['--alluredir={}'.format(report_path)])
-    os.system('cd reports')
-    os.system('allure serve {}'.format(report_dir))
+    os.system('allure serve {}'.format(report_path))
